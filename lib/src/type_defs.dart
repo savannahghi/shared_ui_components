7bfa2eeef10fbd4950748e0b3099e101139ccca5
Vincent Joseph Michuki <vincentmichuki@savannahinformatics.com>
2021-03-20 12:02:43 +0300

    Update lib/src/type_defs.dart

--- lib/src/type_defs.dart
+++ lib/src/type_defs.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 /// callback that is passed to a form fields [onChanged] of [onValue]
-typedef FormFieldCallback = dynamic Function(dynamic value);
+typedef FormFieldCallback = String? Function(dynamic value);
 
 typedef SettingsFunc = Future<void> Function(
     {@required bool value, @required BuildContext context});
