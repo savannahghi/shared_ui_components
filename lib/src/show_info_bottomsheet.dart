557dfd2f5c2a73bd90062169d3b83327d5b095b9
Abiud <abiud.orina@savannahinformatics.com>
2021-04-22 17:26:59 +0300

    fox: conditionally format dropdown options in select option field
    Signed-off-by: Abiud <abiud.orina@savannahinformatics.com>

---
+++ lib/src/show_info_bottomsheet.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:sil_themes/spaces.dart';
+import 'package:sil_themes/text_themes.dart';
+
+class ShowInfoBottomSheet {
+  Future<dynamic> showFeedbackBottomSheet({
+    required BuildContext context,
+    required String modalContent,
+    required String imageAssetPath,
+  }) async {
+    return showModalBottomSheet(
+        context: context,
+        backgroundColor: Colors.transparent,
+        barrierColor: Colors.transparent,
+        isScrollControlled: true,
+        builder: (BuildContext context) {
+          return Container(
+            padding: MediaQuery.of(context).viewInsets,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+              constraints: const BoxConstraints(
+                maxWidth: 420,
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+              margin: const EdgeInsets.all(20),
+              child: SingleChildScrollView(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Flexible(
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          GestureDetector(
+                            child: SvgPicture.asset(
+                              imageAssetPath,
+                              height: 34.0,
+                              width: 34.0,
+                            ),
+                          ),
+                          mediumHorizontalSizedBox,
+                          Flexible(
+                              child: Text(
+                            modalContent,
+                            style: TextThemes.normalSize14Text(),
+                          )),
+                        ],
+                      ),
+                    ),
+                    smallHorizontalSizedBox,
+                    Container(
+                      margin: const EdgeInsets.only(top: 4),
+                      child: GestureDetector(
+                        onTap: () => Navigator.pop(context),
+                        child: const Image(
+                          key: Key('pop_bottomsheet'),
+                          image: AssetImage('assets/icons/close.png'),
+                          color: Colors.black54,
+                          height: 16.0,
+                          width: 16.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
+  }
+}
